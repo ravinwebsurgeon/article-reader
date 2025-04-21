d3df1899eae75d1742c385b5cc34f78cea3cb904
ravin <ravinwebsurgeon@gmail.com>
2025-04-21 20:31:16 +0530

    remove watermelon

--- database/database.ts
+++ database/database.ts
@@ -1,22 +1,21 @@
-import { Database } from '@nozbe/watermelondb';
-import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
-import { Platform } from 'react-native';
+import { Database } from "@nozbe/watermelondb";
+import SQLiteAdapter from "@nozbe/watermelondb/adapters/sqlite";
+import { Platform } from "react-native";
 
-
-import migrations from './migration/index';
-import { ItemModel } from './models/ItemModel';
-import {  UserModel } from './models/UserModel';
-import { schemas } from './schemas';
-import { TagModel } from './models';
+import migrations from "./migration/index";
+import { ItemModel } from "./models/ItemModel";
+import { UserModel } from "./models/UserModel";
+import { schemas } from "./schemas";
+import { TagModel } from "./models";
 
 const adapter = new SQLiteAdapter({
   schema: schemas,
   migrations,
-//   jsi: Platform.OS === 'ios', // Enable JSI on iOS for better performance
+  //   jsi: Platform.OS === 'ios', // Enable JSI on iOS for better performance
   jsi: false,
-  onSetUpError: error => {
-    console.error('Database setup error:', error);
-  }
+  onSetUpError: (error) => {
+    console.error("Database setup error:", error);
+  },
 });
 
 export const database = new Database({
