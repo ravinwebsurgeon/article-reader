91b49e9dc8df20e69501c670d427765ebb82de40
Nick Chapman <nchapman@fastmail.com>
2025-06-16 12:02:24 -0700

    Move iOS share extension into an expo plugin.

--- share-extension-template/Folio/TokenManager.m
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RCT_EXTERN_MODULE(TokenManager, NSObject)
-
-RCT_EXTERN_METHOD(saveToken:(NSString *)token)
-RCT_EXTERN_METHOD(getToken:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(removeToken)
-
-@end
