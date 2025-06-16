2655f206d9e4ed3943bf4218ec48036f5ca2332f
Nick Chapman <nchapman@fastmail.com>
2025-06-16 14:28:08 -0700

    Clean up.

--- index.share.js
+++
@@ -1,3 +0,0 @@
-import { AppRegistry } from "react-native";
-import ShareExtension from "./ShareExtension"; // Your custom component
-AppRegistry.registerComponent("shareExtension", () => ShareExtension);
